884f60deadb09d74131f0103b725eec6468b84f4
Felipe Mendes <felipemendes@me.com>
2019-11-23 22:47:08 -0300

    Setup Xcode project

---
+++ Chuck-Norris-Facts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chuck-Norris-Facts
+//
+//  Created by Felipe Mendes on 23/11/19.
+//  Copyright © 2019 Felipe Mendes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    let mainViewController = ViewController()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
