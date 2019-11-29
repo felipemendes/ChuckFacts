ae28f114cb3bed0416711d41899ae9940a870e19
Felipe <felipemendes@me.com>
2019-11-29 00:22:46 -0300

    Add router request with protocol Routable

---
+++ ChuckService/Router.swift
@@ -0,0 +1,41 @@
+//
+//  Router.swift
+//  ChuckFacts
+//
+//  Created by Felipe Mendes on 28/11/19.
+//  Copyright © 2019 Felipe Mendes. All rights reserved.
+//
+
+import Foundation
+
+class Router<Endpoint: RequestType>: Routable {
+
+    private var task: URLSessionTask?
+
+    func request(_ route: Endpoint, completion: @escaping RouterCompletion) {
+        let session = URLSession.shared
+
+        do {
+            let request = try self.buildRequest(from: route)
+            task = session.dataTask(with: request, completionHandler: { data, response, error in
+                completion(data, response, error)
+            })
+        } catch {
+            completion(nil, nil, error)
+        }
+        self.task?.resume()
+    }
+
+    func cancel() {
+        self.task?.cancel()
+    }
+
+    private func buildRequest(from endpoint: Endpoint) throws -> URLRequest {
+        var request = URLRequest(url: endpoint.baseURL.appendingPathComponent(endpoint.path),
+                                 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                 timeoutInterval: 10.0)
+
+        request.httpMethod = endpoint.httpMethod.rawValue
+        return request
+    }
+}
