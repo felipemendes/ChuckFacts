67afa8fe827ac9a2ca238360e9d34d6989c2a403
Felipe <felipemendes@me.com>
2021-05-12 15:25:42 -0300

    Update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,4 +1,7 @@
 import Danger 
 let danger = Danger()
 
-danger.message("Hello from Danger")
+let prTitle = danger.github.pullRequest.title
+if prTitle.contains("WIP") {
+    warn("PR is classed as _Work in Progress_.")
+}
